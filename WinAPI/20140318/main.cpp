6497a1610304e3973877cd1e26fdb537366a07a7
jklim <gocobe1228@naver.com>
2014-03-18 22:17:21 +0900

    init commit
    초기 등록

---
+++ WinAPI/20140318/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <conio.h>
+#include <tchar.h>
+
+using namespace std;
+
+int foo(void)
+{
+	cout << "hello" << endl;
+
+	return 0;
+}
+
+int main(void)
+{
+	typedef int(*FunctionPointer)(void);
+
+	FunctionPointer a = &foo;
+
+	(*a)();
+
+
+	_getch();
+	return 0;
+}
