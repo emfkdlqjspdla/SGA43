939c80b1649f32eb0dbd550aece7719c57159d3c
jklim <gocobe1228@naver.com>
2014-03-25 22:01:59 +0900

    20140325 일자 코드 추가.
    사운드 처리 추가.
    원 그리는 클래스 구현.

--- WinAPI/20140318/main.cpp
+++ WinAPI/20140318/main.cpp
@@ -9,26 +9,78 @@ using namespace std;
 
 struct ItemInfo
 {
-	char* name[20];
+	char name[20];
 	int price;
 };
 
 int main(void)
 {
-	//ItemInfo item;
+	char* menu[] = {
+		"0. exit",
+		"1. load",
+		"2. save"
+	};
 
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	cout << "name : ";
-	//	cin >> item.name;
-	//	cout << "price : ";
-	//	cin >> item.price;
+	ItemInfo item;
 
-	//	// write to file
-	//}
+	bool bExit = false;
+	while (!bExit)
+	{
+		// menu print
+		for (int i = 0; i < 3; i++)
+		{
+			cout << menu[i] << endl;
+		}
+		int input;
+		cout << "select : ";
+		cin >> input;
 
-	// read from file.
+		switch (input)
+		{
+		case 0 :
+			{
+				bExit = true;
+			}
+			break;
+		case 2 :
+			{
+				fstream outfile;
+				outfile.open("itemlist.dat", ios_base::out | ios_base::binary);
+				for (int i = 0; i < 2; i++)
+				{
+					cin >> item.name;
+					cin >> item.price;
 
+					outfile.write((const char*)&item, sizeof(ItemInfo));
+
+				}
+
+				outfile.close();
+			}
+			break;
+		case 1 :
+			{
+				fstream infile;
+				infile.open("itemlist.dat", ios_base::in | ios_base::binary);
+				for (int i = 0; i < 2; i++)
+				{
+					infile.read((char*)&item, sizeof(ItemInfo));
+
+					cout << item.name << " ";
+					cout << item.price << endl;
+				}
+
+				infile.close();
+			}
+			break;
+		default :
+			{
+			}
+			break;
+		}
+
+
+	}
 
 	fstream file;
 
