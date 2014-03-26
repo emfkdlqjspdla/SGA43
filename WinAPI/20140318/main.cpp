5d5fef11dc53894d85f5a39a875d9c16824adae8
jklim <gocobe1228@naver.com>
2014-03-26 21:59:24 +0900

    20140326 추가
    ...

--- WinAPI/20140318/main.cpp
+++ WinAPI/20140318/main.cpp
@@ -3,94 +3,25 @@
 #include <tchar.h>
 #include <cmath>
 #include <list>
-#include <fstream>
 
 using namespace std;
 
-struct ItemInfo
-{
-	char name[20];
-	int price;
-};
-
 int main(void)
 {
-	char* menu[] = {
-		"0. exit",
-		"1. load",
-		"2. save"
-	};
+	list<int> test;
 
-	ItemInfo item;
+	for (int i = 0; i < 10; i++)
+		test.push_back(i);
 
-	bool bExit = false;
-	while (!bExit)
+	list<int>::iterator it;
+	for (it = test.begin(); it != test.end(); it++)
 	{
-		// menu print
-		for (int i = 0; i < 3; i++)
-		{
-			cout << menu[i] << endl;
-		}
-		int input;
-		cout << "select : ";
-		cin >> input;
-
-		switch (input)
-		{
-		case 0 :
-			{
-				bExit = true;
-			}
-			break;
-		case 2 :
-			{
-				fstream outfile;
-				outfile.open("itemlist.dat", ios_base::out | ios_base::binary);
-				for (int i = 0; i < 2; i++)
-				{
-					cin >> item.name;
-					cin >> item.price;
-
-					outfile.write((const char*)&item, sizeof(ItemInfo));
-
-				}
-
-				outfile.close();
-			}
-			break;
-		case 1 :
-			{
-				fstream infile;
-				infile.open("itemlist.dat", ios_base::in | ios_base::binary);
-				for (int i = 0; i < 2; i++)
-				{
-					infile.read((char*)&item, sizeof(ItemInfo));
-
-					cout << item.name << " ";
-					cout << item.price << endl;
-				}
-
-				infile.close();
-			}
-			break;
-		default :
-			{
-			}
-			break;
-		}
-
-
+		cout << *it << endl;
 	}
 
-	fstream file;
 
-	file.open("data.dat", ios_base::out | ios_base::binary);
-
-	int a = 11;
-
-	file.write((char*)&a, sizeof(int));
-
-	file.close();
+	for (int i = 0; i < 10; i++)
+		test.pop_front();
 
 	_getch();
 	return 0;
