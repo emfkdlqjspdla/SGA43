f10169f403d349ec64ac91ace3d54abde05ae882
jklim <gocobe1228@naver.com>
2014-03-31 21:23:04 +0900

    20140331 test
    ..

--- WinAPI/20140331/CannonApp.h
+++ WinAPI/20140331/CannonApp.h
@@ -4,4 +4,31 @@
 
 class CannonApp : public MainWindow<CannonApp>
 {
+	typedef CannonApp Me;
+	typedef MainWindow<CannonApp> Base;
+public :
+protected :
+	void SetEventMap()
+	{
+		Base::SetEventMap();
+
+		AddEventHandler(WM_PAINT, &Me::OnPaint);
+	}
+	LRESULT OnPaint(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		RECT rc;
+		::GetClientRect(hWnd, &rc);
+
+		PAINTSTRUCT ps;
+		HDC hdc = ::BeginPaint(hWnd, &ps);
+
+		::DrawText(hdc, _T("Draw Bitmap"), -1, &rc, DT_TOP);
+
+
+		::EndPaint(hWnd, &ps);
+
+		return 0;
+	}
+private :
+
 };
